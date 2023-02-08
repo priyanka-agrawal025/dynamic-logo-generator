a47932b4332e0d6fdf18156f6c859c74ce8e1bdc
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-02-07 21:22:49 -0500

    added notes to the js files to plan out functions

---
+++ index.js
@@ -0,0 +1,9 @@
+// Import Inquirer and Node File System
+// Import the "shape.js" file
+// Write prompts which the user will answer
+    // Prompt for 3 characters of text
+    // Prompt for text color
+        // Try finding a RegEx for hex colors
+    // Prompt for shape (CIRCLE, SQUARE, TRIANGLE)
+    // Prompt for shape color
+// Console.log "Genrated logo.svg" in the terminal when all the prompts are answered

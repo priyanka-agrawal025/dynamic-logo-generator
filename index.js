71e0b2fc2e5e42f6f87da38abd5099b32be35499
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-02-11 14:49:00 -0500

    added prompts, shapes functions, and shape rendering ability to application

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-// Import Inquirer and Node File System
-// Import the "shape.js" file
-// Write prompts which the user will answer
-    // Prompt for 3 characters of text
-    // Prompt for text color
-        // Try finding a RegEx for hex colors
-    // Prompt for shape (CIRCLE, SQUARE, TRIANGLE)
-    // Prompt for shape color
-// Console.log "Genrated logo.svg" in the terminal when all the prompts are answered
+const fs = require('fs/promises');
+const PROMPTS = require('./lib/prompts');
+const SHAPES = require('./lib/shapes');
+
+const prompt = new PROMPTS();
+
+function createLogo(userInput) {
+    let logo = '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">';
+
+    const shapes = new SHAPES(userInput.shapeColor, userInput.shape);
+    
+    logo += shapes.getShape();
+
+    logo += `<text x="110" y="110" font-size="40" fill="${userInput.textColor}">${userInput.text}</text>`
+
+    logo += '</svg>';
+    // console.log(logo);
+    fs.writeFile('logo.svg', logo) 
+    .then(() => {
+        console.log("Generated logo.svg!!")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+prompt.run()
+.then(()=> {
+    createLogo(prompt.answers);
+})
